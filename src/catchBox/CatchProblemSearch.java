b78c1e6cbc287087b34f983969cb1d410586b9fa
Miguel Alendouro <miguelalendouro@gmail.com>
2019-05-18 19:06:56 +0100

    CatchProblemSearch [DONE]
    -> The class CatchProblemSearch is now all implemented 
    -> Just left heuristic class to implment

--- src/catchBox/CatchProblemSearch.java
+++ src/catchBox/CatchProblemSearch.java
@@ -4,36 +4,47 @@ import agentSearch.Action;
 import agentSearch.Problem;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CatchProblemSearch<S extends CatchState> extends Problem<S> {
     //TODO this class might require the definition of additional methods and/or attributes
 
-    private final CatchState goalState;
+    private LinkedList<Action> actions;
+
+    //private final CatchState goalState;
+    //  Create a Catch where we set the goal to the goalPosition (Cell) and then in the isGoal function we just do a get of the line of the goal state abd cnoare it (but it would take more lines of code)
+    private Cell goalPosition;
 
     public CatchProblemSearch(S initialCatchState, Cell goalPosition) {
         super(initialCatchState);
 
-        initialCatchState.setGoal(goalPosition.getLine(), goalPosition.getColumn());
-        this.goalState = new CatchState(initialCatchState.getMatrix());
+        actions = new LinkedList<Action>() {{
+            add(new ActionDown());
+            add(new ActionRight());
+            add(new ActionUp());
+            add(new ActionLeft());
+        }};
+
+        this.goalPosition = goalPosition;
     }
 
     @Override
     public List<S> executeActions(S state) {
-        //TODO
+
         List<S> successors = new ArrayList<>();
-        for (Action action : availableActions) {
+
+        for (Action action : actions) {
             if(action.isValid(state)) {
                 S successor = (S) state.clone();
                 action.execute(successor);
                 successors.add(successor);
             }
         }
-        //TODO
         return successors;
     }
 
     public boolean isGoal(S state) {
-        return goalState.getColumnGoal() == state.getCatchColumn() && goalState.getLineGoal() == state.getCatchLine();
+        return goalPosition.getColumn() == state.getCatchColumn() && goalPosition.getLine() == state.getCatchLine();
     }
 }
